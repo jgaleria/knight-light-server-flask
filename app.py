d0f83222cb40bd439833049f9199cfcc70c9ba15
Joshua Galeria <jgaleriacs@gmail.com>
2022-06-30 08:31:00 -1000

    Finalize status

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ from flask import Flask
 app = Flask(__name__)
 
 #Creating Data Schema
-status = ["ON", "OFF"]
+status = "OFF"
 
 #Return current status of signal
 @app.route('/', methods=['GET'])
 def index():
-    return "This is the status of the webpage: "
+    return f"This is the status of the webpage: {status}"
 
 #Turn status on
 @app.route('/activate', methods = ['POST'])
 def activate():
-    status = ["ON"]
-    return "The status was just switched to " + status
+    status = "ON"
+    return f"The status was just switched to {status}"
 
 #Turn status off
 @app.route('/deactivate', methods = ['POST'])
 def deactivate():
-    status = ["OFF"]
-    return "The status was just switched to " + status
+    status = "OFF"
+    return f"The status was just switched to {status}" 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
